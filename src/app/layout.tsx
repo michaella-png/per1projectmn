b6476ac224e93cef1d9734e3c1e2e9e1c747d8b5
Michaella Napier <2421400515@k12.hi.us>
2024-09-10 09:01:44 -1000

    dessert card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,29 @@ export default function RootLayout({
   return (
     <html lang="en">
        <body className={`${GeistSans.variable}`}>
-        <div className="w-full text-9xl text-pink-500 text-center">First Layout</div>
+       <div className="w-full text-9xl text-orange-500 text-center">First Layout</div>
+       <button className="btn btn-outline bg-pink-200 text-rose-400">Boop</button>
+
+       <div className="card bg-yellow-100 w-80 shadow-xl ">
+          <figure>
+            <img
+              src="https://utfs.io/f/556971d5-4e5f-43d8-a4bc-454c055591bc-nm33wo.jpg"
+              alt="Mango Sticky Rice" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-orange-400">Mango Sticky Rice!</h2>
+            <p className="text-yellow-500">Get me this rn. </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-default  bg-yellow-200 text-orange-400">Order Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">Accept viruses ^~^</span>
+            <input type="checkbox" defaultChecked className="checkbox" />
+          </label>
+        </div>
       {children}
     </body>
     </html>
